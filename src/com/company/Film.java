3f950a301595cff07f220616e67a6ae77e188d9c
Popeye985 <ceulemans_koen@hotmail.com>
2018-08-31 01:05:29 +0200

    FinishedVersionLast

--- src/com/company/Film.java
+++ src/com/company/Film.java
@@ -13,20 +13,14 @@ public class Film {
     public static void main(String[] args) throws IOException {
 
         int keuze = 0;
-        ArrayList<FilmLijst> titels = new ArrayList<>();
-        titels.add(new FilmLijst("Rise of the Guardians"));
-        titels.add(new FilmLijst("K3 en het magische Medaillon"));
-        titels.add(new FilmLijst("K3 en het ijsprinsesje"));
-        titels.add(new FilmLijst("K3 en de kattenprins"));
-        titels.add(new FilmLijst("K3 Bengeltjes"));
-        titels.add(new FilmLijst("Schindler's List"));
-        titels.add(new FilmLijst("Total Recall"));
-        titels.add(new FilmLijst("Bambi"));
+        ArrayList<FilmLijst> titels = FilmLijst.getFilmLijsts();
         Collections.sort(titels);
-        FilmLijst filmOpvragen = new FilmLijst("");
+        ArrayList<FilmLijst> releaseJaar = FilmLijst.getReleasejaar();
+        ArrayList<FilmLijst> regisseur = FilmLijst.getRegisseur();
+
 
         do {
-            toonMenu();
+            FilmLijst.toonMenu();
             Scanner invoer = new Scanner(System.in);
             try {
                 keuze = Integer.parseInt(invoer.nextLine());
@@ -37,10 +31,10 @@ public class Film {
 
             switch (keuze) {
                 case 1:
-                    filmsOpvragen(titels, invoer, filmOpvragen);
+                    filmsOpvragen(titels, invoer);
                     break;
                 case 2:
-                    filmToevoegen(titels, invoer);
+                    FilmLijst.filmToevoegen(titels, invoer);
                     break;
                 case 3:
                     filmsVerwijderen(titels, invoer);
@@ -49,26 +43,28 @@ public class Film {
                     try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("tekst.txt")))) {
                         for (int i = 0; i < titels.size(); i++) {
                             writer.printf("%d. %s %n", i + 1, titels.get(i));
-
                         }
                     }
                     break;
-
                 case 5:
-                    stopProgramma(keuze);
+                    detailsOpvragen(titels, releaseJaar, invoer);
+                    break;
+                case 6:
+                    FilmLijst.stopProgramma(keuze);
                     break;
             }
-
-        } while (keuze < 4 && keuze != 1);
-
+        } while (keuze <= 6 && keuze != 1);
     }
 
 
-    private static void stopProgramma(int keuze) {
-        if (keuze >= 5) {
-            System.out.println("Einde programma");
-        }
-        return;
+    private static void detailsOpvragen(ArrayList<FilmLijst> titels, ArrayList<FilmLijst> releaseJaar, Scanner invoer) {
+        filmlijst(titels);
+        System.out.println("Van welke film wilt u details zien? Geef het overeenkomstige nummer in.");
+        int filmDetails = Integer.parseInt(invoer.nextLine());
+        FilmLijst gekozenDetails = releaseJaar.get(filmDetails - 1);
+        FilmLijst gekozenDetailsreg = FilmLijst.getRegisseur().get(filmDetails-1);
+        System.out.printf("Het releasejaar van %s is: %s%n",filmDetails, gekozenDetails);
+        System.out.printf("De regisseur van %s is: %s%n", filmDetails, gekozenDetailsreg );
     }
 
 
@@ -96,19 +92,7 @@ public class Film {
         } while (!titelverWijderen.equals(""));
     }
 
-    private static void filmToevoegen(ArrayList<FilmLijst> titels, Scanner invoer) {
-        String titel;
-        do {
-            System.out.println("Geef een titel in");
-            titel = invoer.nextLine();
-            if (!titel.equals(""))
-                titels.add(new FilmLijst(titel));
-        } while (!titel.equals(""));
-    }
-
-
-    private static void filmsOpvragen(ArrayList<FilmLijst> titels, Scanner invoer, FilmLijst filmOpvragen) {
-        filmOpvragen.getTitel();
+    private static void filmsOpvragen(ArrayList<FilmLijst> titels, Scanner invoer) {
         filmlijst(titels);
         System.out.println("Wilt u een film bekijken? Geef ja of nee in");
         String filmKeuze = invoer.nextLine();
@@ -121,6 +105,7 @@ public class Film {
                     filmSelecteren = Integer.parseInt(invoer.nextLine());
                     FilmLijst gekozenTitel = titels.get(filmSelecteren - 1);
                     System.out.printf("De gekozen film is \"%s.\"", gekozenTitel);
+                    return;
 
                 } else if (filmKeuze.equalsIgnoreCase("NEE")) {
                     System.out.printf("Einde programma");
@@ -131,17 +116,6 @@ public class Film {
             }
         }
     }
-
-
-    private static void toonMenu() {
-        System.out.println("Wat wilt u doen?");
-        System.out.println("1. Filmlijst opvragen");
-        System.out.println("2. Een film toevoegen. <Return> om te stoppen");
-        System.out.println("3. Een film verwijderen. <Return om te stoppen");
-        System.out.println("4. Een tekstbestand met films genereren");
-        System.out.println("5. Stoppen");
-    }
-
 
     private static void filmlijst(ArrayList<FilmLijst> titels) {
         System.out.println("Selecteer uw film");
@@ -155,25 +129,97 @@ public class Film {
 
 class FilmLijst implements Comparable<FilmLijst> {
     private String titel;
+    private String regisseur;
+    private int releaseJaar;
+
+    public FilmLijst(String titel) {
+        this.titel = titel;
+
+    }
+
+    public FilmLijst() {
+
+    }
 
     @Override
     public String toString() {
         return titel;
     }
 
-    public FilmLijst(String titel) {
-        this.titel = titel;
-
-
-    }
 
     public String getTitel() {
         return titel;
     }
 
-
     @Override
     public int compareTo(FilmLijst o) {
         return this.titel.compareTo(o.titel);
+    }
+
+    public static void toonMenu() {
+        System.out.println("Wat wilt u doen?");
+        System.out.println("1. Filmlijst opvragen");
+        System.out.println("2. Een film toevoegen. <Return> om te stoppen");
+        System.out.println("3. Een film verwijderen. <Return om te stoppen");
+        System.out.println("4. Een tekstbestand met films genereren");
+        System.out.println("5. Details bekijken");
+        System.out.println("6. Stoppen");
+    }
+
+    public static ArrayList<FilmLijst> getReleasejaar() {
+        ArrayList<FilmLijst> releaseJaar = new ArrayList<>();
+        releaseJaar.add(new FilmLijst("1942"));
+        releaseJaar.add(new FilmLijst("2012"));
+        releaseJaar.add(new FilmLijst("2007"));
+        releaseJaar.add(new FilmLijst("2006"));
+        releaseJaar.add(new FilmLijst("2004"));
+        releaseJaar.add(new FilmLijst("2012"));
+        releaseJaar.add(new FilmLijst("1943"));
+        releaseJaar.add(new FilmLijst("1990"));
+        return releaseJaar;
+    }
+
+    public static ArrayList<FilmLijst> getFilmLijsts() {
+        ArrayList<FilmLijst> titels = new ArrayList<>();
+        titels.add(new FilmLijst("Rise of the Guardians"));
+        titels.add(new FilmLijst("K3 en het magische Medaillon"));
+        titels.add(new FilmLijst("K3 en het ijsprinsesje"));
+        titels.add(new FilmLijst("K3 en de kattenprins"));
+        titels.add(new FilmLijst("K3 Bengeltjes"));
+        titels.add(new FilmLijst("Schindler's List"));
+        titels.add(new FilmLijst("Total Recall"));
+        titels.add(new FilmLijst("Bambi"));
+        return titels;
+    }
+
+    public static ArrayList<FilmLijst> getRegisseur() {
+        ArrayList<FilmLijst>getRegisseur = new ArrayList<>();
+        getRegisseur.add(new FilmLijst("David Hand"));
+        getRegisseur.add(new FilmLijst("Bart van Leemputten"));
+        getRegisseur.add(new FilmLijst("Matthias Temmermans"));
+        getRegisseur.add(new FilmLijst("Indra Siera"));
+        getRegisseur.add(new FilmLijst("Indra Siera"));
+        getRegisseur.add(new FilmLijst("Peter Ramsey"));
+        getRegisseur.add(new FilmLijst("Steven Spielberg"));
+        getRegisseur.add(new FilmLijst("Paul Verhoeven"));
+        return getRegisseur;
+    }
+
+    public static void filmToevoegen(ArrayList<FilmLijst> titels, Scanner invoer) {
+        String titel;
+        do {
+            System.out.println("Geef een titel in");
+            titel = invoer.nextLine();
+            if (!titel.equals(""))
+                titels.add(new FilmLijst(titel));
+        } while (!titel.equals(""));
+
+    }
+
+    public static void stopProgramma(int keuze) {
+        if (keuze >= 6) {
+            System.out.println("Einde programma");
+        }
+        return;
     }
 }
